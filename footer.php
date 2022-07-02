7deabc94695cce68bfe8b48c8d842581e5432e1f
mehedihasanreal <mehedihasan6002@gmail.com>
2022-07-02 19:15:08 +0600

    Hospital Management System

---
+++ footer.php
@@ -0,0 +1,28 @@
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© <?php echo date("Y"); ?> Hospital Management System & Application Developed By <span style="color: white">Md. Mehedi Hasan Real</span></p>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,155 @@
+
+  <?php include 'header.php';?>
+  
+  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+            <!-- Intro Detail -->
+            <div class="col-md-8">
+              <div class="text-sec">
+                <h5>Health Check Ups</h5>
+                <p>Besides providing world class clinical lab services, Labaid Diagnostic Centre houses a pool of doctors of different medical specialty to serve the ailing patients as outpatients. They are all reputed and respected in their medical specialty for outstanding clinical management</p>
+                <ul class="row">
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
+            <!-- Intro Timing -->
+            <div class="col-md-4">
+              <div class="timing"> <i class="lnr lnr-clock"></i>
+                <ul>
+                  <li> Monday <span>8.00 - 16.00</span></li>
+                  <li> Tuesday <span>8.00 - 16.00</span></li>
+                  <li> Wednesday <span>8.00 - 16.00</span></li>
+                  <li> Thursday <span>8.00 - 16.00</span></li>
+                  <li> Friday <span>8.00 - 16.00</span></li>
+                  <li> Saturday <span>8.00 - 16.00</span></li>
+                  <li> Sunday <span>8.00 - 16.00</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  
+
+    
+    <!-- DOCTOR LIST -->
+    <section class="p-t-b-150">
+      <div class="container"> 
+        
+        <!-- Heading -->
+        <div class="heading-block">
+          <h2>Our Services</h2>
+          <hr>
+          <span>Duis autem vel eum iriure dolor in hendrerit n vuew lputate velit esse molestie conseu vel illum dolore eufe ugiat nulla facilisis at vero.</span> </div>
+        
+        <!-- Services -->
+        <div class="services">
+          <div class="row"> 
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-eye-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Eye Specialist</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-operating-room icon"></i> </div>
+                <div class="media-body">
+                  <h6>Operation Theater</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-icu-monitor icon"></i> </div>
+                <div class="media-body">
+                  <h6>ICU Department</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-doctor icon"></i> </div>
+                <div class="media-body">
+                  <h6>Qualified Doctors</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-heartbeat icon"></i> </div>
+                <div class="media-body">
+                  <h6>Heart Problems</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-stomach-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Stomach Problems</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+   
+    
+    
+  </div>
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
